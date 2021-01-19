d8cc7d3573f909a376e1ef4b2f69cdf340099419
Cristian Metz <cristian@mobilityps.com>
2021-01-19 18:23:52 -0300

    various updates

--- hello world.php
+++ hello world.php
@@ -3,49 +3,41 @@
 <head>
 <title>Page Title</title>
 </head>
-<link href="bootstrap5/css/bootstrap.min.css" rel="stylesheet" >
-<script src="bootstrap5/js/bootstrap.bundle.min.js" ></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <body>
-    <style type="text/css">
-        .row>* ,.row 
-        {
-            padding-right: unset;
-            padding-left: unset;
-            margin-top: unset;
-            margin-right: unset;
-            margin-left: unset;
-        }
-    </style>
 <script>
-    
     //This makes the overlay
-    $("body").append('<div class="row" id="overlay" style="position: absolute;"><span class="col" style="font-size: xx-large;margin-left: 0.5em;font-weight: bold;color: antiquewhite;"id="score">aaaa</span></div>')
     //variable initialization
     p=0
-    height=9
-    width=35
+    width=100 //first max: 20 , post optimization 200 runs smooth as fuck and is probably my favourite, but it does not crash even if you go higher, its just too good
+    tilewidth=globalThis.outerWidth/width
+    height=0
+    while((height*tilewidth+tilewidth)<$(window).height()){
+        height++
+    }
+    height--
+    playercolor='ffffff'
     tickspeed=100
     stop=false    
     score=0
-    difficulty=5
+    difficulty=8
+    console.log()
     var grid=[]
+    initialbody=''
     //creating the grid
+    $("body").append('<div class="row" id="overlay" style="position: absolute;"><span class="col" style="font-size: xx-large;margin-left: 0.5em;font-weight: bold;color: antiquewhite;"id="score">aaaa</span></div>')
+    
     for(y=0;y<=height;y++){
         grid.push([]);
-        $("body").append("<div class='row' id='"+y+"'>")
+        initialbody=initialbody+("<div id='row"+y+"' style='display:flex'>")   
         for (x=0;x<=width;x++){
-            $("#"+y+"").append("<div class='col-sm' id='"+y+x+"'> </div>")
+            initialbody=initialbody+("<div class='all' id='col"+y+'-'+x+"' style='width: "+tilewidth+"px; display:flex;height:"+tilewidth+"px;'> </div>")
             grid[y][x]=0;
         }
+        initialbody=initialbody+"</div>"
     }
-    cw = $('#00').width()
-    for(y=0;y<=height;y++){
-        for (x=0;x<=width;x++){
-            $('#'+y+x).css({'height':cw+'px'})
-        }
-    }
+    $("body").append(initialbody);
     //This sets the tickspeed. Enemies and things apart from the player will act by this.
     setInterval(forcedUpdate, tickspeed);
     //This sets the score timer.
@@ -56,7 +48,6 @@
     //1 means player
     //CONTROLS
     document.onkeydown = checkKey;
- 
     function checkKey(e) {
         e = e || window.event;
         if (e.keyCode == '38') {
@@ -71,8 +62,11 @@
         else if (e.keyCode == '39') {
             movement('right')
         }
+        else if (e.keyCode == '78') {
+            init()
+        }
     }
-    function scoreUpdate(){
+    async function scoreUpdate(){
         if (!stop){
             score++
             document.getElementById("score").textContent=score;
@@ -84,18 +78,15 @@
     }
     function forcedUpdate() {
         if (!stop){
-            $('.col-sm').css({'background-color':'black'})
             allentities=findAllEntities()
             allentities.forEach( function(entity, indice, allentities) {
-                if (entity[2]==2){
-                    
+                if (entity[2]==2){    
                     if (entity[0]+1>height){
                         grid[entity[0]][entity[1]]=0
                     }else{
                         if(grid[entity[0]+1][entity[1]]==1){
                             collision(grid[entity[0]][entity[1]])
                             grid[entity[0]][entity[1]]=0
-                            playercolor='ff0000'
                         }else{
                             grid[entity[0]][entity[1]]=0
                             grid[entity[0]+1][entity[1]]=2
@@ -108,30 +99,15 @@
                     generateEnemy()
                 }
             }
-            for(y=0;y<=height;y++){
-                for(x=0;x<=width;x++){
-                    if (grid[y][x]!=0){
-                        switch(grid[y][x]){
-                            case 1:
-                                document.getElementById(y+''+x).style.setProperty('background-color', "#"+playercolor, 'important');
-                            break;
-                            case 2:
-                                document.getElementById(y+''+x).style.setProperty('background-color', "grey", 'important');
-                            break;
-                        }
-                    }
-                }
-            }
-            playercolor='ffffff'
+            render()            
         }else{
             if(p==0){
+                render()
                 yandx=findEntity(1)
-                document.getElementById(yandx[0]+''+yandx[1]).style.setProperty('background-color', "red", 'important');
                 console.log(yandx)
                 console.table(grid);
                 p++
             }
-            
         }
     }
     function findEntity(entity){
@@ -156,6 +132,36 @@
             }
         }
         return entities
+    }
+    async function render(initial=false){
+            for(y=0;y<=height;y++){
+                for(x=0;x<=width;x++){
+                    switch(grid[y][x]){
+                        case 1:
+                            document.getElementById('col'+y+'-'+x).style.setProperty('background-color', "#"+playercolor, 'important');                                
+                        break;
+                        case 2:
+                            document.getElementById('col'+y+'-'+x).style.setProperty('background-color', "grey", 'important');
+                        break;
+                        default:
+                            document.getElementById('col'+y+'-'+x).style.setProperty('background-color', "black", 'important');
+                        break;
+                    }    
+                }
+            }
+    }
+    function init(){
+        for(y=0;y<=height;y++){
+            for (x=0;x<=width;x++){
+                grid[y][x]=0;
+            }
+        }
+        //This sets the initial player position
+        grid[height][Math.round(width/2)]=1;
+        playercolor='ffffff'
+        score=0
+        stop=false
+        p=0
     }
     function movement(direction) {
         yandx=findEntity(1)
@@ -204,19 +210,8 @@
         }
     }
     function collision(entity){
+        playercolor='ff0000'
         stop=true
-        console.log('Colision con: '+entity);
-    }
-    function log(log){
-        $("#log").append(log);
-        console.log(log);
-    }
-    function showLog(show=true){  
-        if (show){
-            document.getElementById('log').style.setProperty('display', 'flex', 'important');
-        }else{
-            document.getElementById('log').style.setProperty('display', 'none', 'important');
-        }
     }
 </script>
 </body>
